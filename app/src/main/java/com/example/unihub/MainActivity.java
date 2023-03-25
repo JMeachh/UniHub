ebae330eb0f6cd05d4a533769a08b6198003ff6c
Jake <jrmoo7@sky.com>
2023-03-25 19:51:32 +0000

    hi

---
+++ app/src/main/java/com/example/unihub/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.unihub;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import com.example.unihub.databinding.ActivityMainBinding;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+        TextView date = findViewById(R.id.date);
+        date.setText(currentDate);
+
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.planning:
+                    replaceFragment(new PlanningFragment());
+                    break;
+                case R.id.social:
+                    replaceFragment(new SocialFragment());
+                    break;
+            }
+            return true;
+        });
+
+    }
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =  fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+
+
+    }
+
+}
